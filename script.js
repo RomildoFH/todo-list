8ec21922bc9cda3915630791b68bd6e1573fa1ca
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-03 19:21:10 -0300

    Concluidos requisitos 7 e 8

--- script.js
+++ script.js
@@ -11,9 +11,26 @@ function criarTarefa () {
     btnAdicionarTarefa.addEventListener('click', function () {
         let tarefa = document.createElement('li');
         tarefa.innerHTML = inputCamp.value;
+        tarefa.className = 'tarefa';
         listaTarefas.appendChild(tarefa);
         inputCamp.value = "";
     })
 }
 criarTarefa ()
+
+//Destacando atividade selecionada com background cinza
+
+function classeTarefaSelecionada () {
+    let tarefa = document.getElementsByClassName('tarefa');
+    listaTarefas.addEventListener('click', function (event) {        
+        for (index = 0; index < tarefa.length; index ++) {
+            tarefa[index].className = 'tarefa';  
+            tarefa[index].style.backgroundColor = 'white';        
+        }        
+        let tarefaSelecionada = event.target
+        tarefaSelecionada.className = 'tarefa selecionada';
+        tarefaSelecionada.style.backgroundColor = 'gray';        
+    });
+}
+classeTarefaSelecionada ();
 
